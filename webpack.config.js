abe63b4b6a1255c9c565e41de423f225b240b529
fengwei <fengwei029@foxmail.com>
2019-07-03 23:32:20 +0800

    add init

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+
+
+module.exports = {
+   entry: {
+      main: [
+         './src/index.js',
+         './src/other.js'
+      ]
+   },
+
+   output: {
+
+   },
+
+   module: {
+      rules: [
+         {}
+      ]
+   },
+
+   plugins: [
+
+   ]
+}
