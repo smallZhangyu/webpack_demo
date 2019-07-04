bad2614f65ba58fe3e9e100618519274c0ea0886
fengwei <fengwei029@foxmail.com>
2019-07-04 21:48:24 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
    entry: {
@@ -14,11 +16,50 @@ module.exports = {
 
    module: {
       rules: [
-         {}
+         // es6 等
+         {
+            test: /\.jsx?/, // 支持 js 和 jsx
+            include: [
+              path.resolve(__dirname, 'src'), // src 目录下的才需要经过 babel-loader 处理
+            ],
+            loader: 'babel-loader',
+          },
+
+         // css 
+         {
+            test: /\.css/,
+            include: [
+               path.resolve(__dirname, 'src'),
+             ],
+            use: ExtractTextPlugin.extract({ 
+               fallback: 'style-loader',
+               use: 'css-loader',
+             })
+         },
+
+         // 图片处理
+         {
+            test: /\.(png|jpg|gif)$/,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {},
+              },
+            ],
+          },
+
+         // 
+
+
       ]
    },
 
    plugins: [
+      new HtmlWebpackPlugin({
+         filename: 'index.html',
+         template: 'public/index.html'
+      }),
 
+      new ExtractTextPlugin('index.css'),
    ]
 }
